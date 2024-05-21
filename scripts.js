d34a0f59049b8af7c82e9adad48f4e76e32bdb93
Noxolo <wendyngcobo98@gmail.com>
2024-05-21 11:13:53 +0200

    bug fix

--- scripts.js
+++ scripts.js
@@ -228,7 +228,7 @@ document.querySelector('[data-search-form]').addEventListener('submit', (event) 
 
     matches = filterBooks(books,filters) // use filterBooks function withh given form data filter
     page = 1;
-    if (result.length < 1) {
+    if (matches.length < 1) {
         document.querySelector('[data-list-message]').classList.add('list__message_show')
     } else {
         document.querySelector('[data-list-message]').classList.remove('list__message_show')
@@ -236,7 +236,7 @@ document.querySelector('[data-search-form]').addEventListener('submit', (event) 
 
     document.querySelector('[data-list-items]').innerHTML = ''
     const newItems = document.createDocumentFragment();
-    const resultSlicedObject = result.slice(0, BOOKS_PER_PAGE);
+    const resultSlicedObject = matches.slice(0, BOOKS_PER_PAGE);
     // replace the for loop  for newItems , use the createBookPreview
     createBookPreview(newItems, resultSlicedObject);
     //call updateshowmore function 
